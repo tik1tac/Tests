993645aaba026aa7f2e982e9c23380172c0e3ee0
иван петров <yurass228@yandex.ru>
2023-10-30 00:56:19 +0300

    TestsTut

---
+++ UnitTest/Migrations/20231029211854_initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UnitTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    AccountNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "AccountNumber", "Age", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("7cb3f24d-b649-461d-8160-80155de416ed"), "123-9384613085-55", 28, "Evelin" },
+                    { new Guid("d9112050-7f20-4af8-a0c9-e685776d9bf1"), "123-3452134543-32", 30, "Mark" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
